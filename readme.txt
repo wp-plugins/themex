4b8dc5929ca045fb1cf67dd38e51dbde31353769
Xnuiem <Xnuiem@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-07 01:53:34 +0000

    0.2
    git-svn-id: https://plugins.svn.wordpress.org/themex/tags/0.2@84760 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,55 @@
+=== ThemeX ===
+Contributors: Xnuiem
+Donate link: http://www.thisrand.com/scripts/themex
+Tags: theme, plugin, admin
+Requires at least: 2.6
+Tested up to: 2.7
+Stable Tag: 0.2
+
+A lightweight plugin that will allow the automatic rotation of a pair of themes based on the time of day.
+
+== Description ==
+This plugin alternates your WP installation between two themes based on the time of day.  The themes and start times
+for each are customizable through an administration interface.
+
+
+== Installation ==
+
+1. Upload the ticketx directory to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Under the Tools menu, go to the 'ThemeX' administration page
+4. Enter in the two themes you wish to use, and the times for starts
+5. Press 'Save Changes' and you are finished.
+
+
+== Frequently Asked Questions ==
+None Yet
+
+
+== Change Log ==
+<b>0.1</b><br />
+Initial Release
+
+<b>0.2</b><br />
+Added Time Zone setting
+Added Date based rotation - Thanks reuben lee b
+Dropped the changeTheme() function to use WP switch_theme()
+
+
+== Screenshots==
+None Yet
+
+
+== Demos ==
+[this.rand()](http://www.thisrand.com) <br />
+
+
+
+
+== To Do ==
+Nothing at this point.
+
+
+
+
+
---
+++ themex_functions.php
@@ -0,0 +1,93 @@
+<?php
+
+class themex_functions {
+	/**
+	* The front-end methods for themeX.
+ 	* @package WordPress
+ 	*/
+
+	function checkTheme(){
+
+		/**
+	 	* Checks to see if the theme/time are in sync, and changes the theme if not.
+	 	*/
+
+		$time = date("H", time());
+		$current = get_current_theme();
+		$options = get_option("themex_options");
+
+		$offset = 0;
+
+		if (count($options) > 3){
+			//Ok, module is active, let's see what to do.
+			if ($options["type"] == "simple"){
+				if ($options["timeZone"]){
+	       	 	$server = date("O");
+        			if ($server != $options["timeZone"]){
+						if ($server < 0){
+							$offset = ($server - $options["timeZone"])/100;
+							if ($options["timeZone"] > 0){
+								$offset = $offset * -1;
+							}
+							$time = $time - $offset;
+						}
+						else {
+							$offset = ($server - $options["timeZone"])/100;
+							if ($options["timeZone"] > 0){
+	                        	$offset = abs($offset);
+							}
+							$time = $time + $offset;
+						}
+        			}
+				}
+
+				if (($time) >= $options['dayStart'] && $current != $options['dayTheme']){
+					//Time is past or equal to day start and current theme is night.
+					//print("Inside 1");
+					switch_theme($options['dayTheme'], $options['dayTheme']);
+					$current = $options['dayTheme'];
+				}
+
+				if (($time) < $options['dayStart'] && $current != $options['nightTheme']){
+					//Time is prior to day start and current theme is day
+					//print("Inside 2");
+					switch_theme($options['nightTheme'], $options['nightTheme']);
+					$current = $options['nightTheme'];
+				}
+
+            	if (($time) >= $options['nightStart'] && $current != $options['nightTheme'] && $options['dayStart'] < $options['nightStart']){
+	            	//Time is after or equal to night start and current theme is day and day start is less than night start
+            		//print("Inside 3");
+            		switch_theme($options['nightTheme'], $options['nightTheme']);
+            		$current = $options['nightTheme'];
+            	}
+            }
+            else if ($options["type"] == "date"){
+            	for($m=1;$m<13;$m++){
+     			   	$fieldName = "theme" . $m;
+     		   		$monthName = "theme" . $m . "-month";
+     			   	$dayName   = "theme" . $m . "-day";
+        			$yearName  = "theme" . $m . "-year";
+        			$timeName  = "theme" . $m . "-time";
+
+            	    $compareDate = $options[$yearName] . $options[$monthName] . $options[$dayName] . $options[$timeName];
+
+            	    if ($options[$dayName] == ''){ break; }
+
+            	    if ($compareDate <= date('YndH') && $compareDate > $checkDate){
+                   		$checkDate  = $compareDate;
+                   		$checkTheme = $options[$fieldName];
+            	    }
+            	    else if ($compareDate > date('YndH')){ break; }
+
+            	}
+
+            	if ($current != $checkTheme){ switch_theme($checkTheme, $checkTheme); }
+            }
+		}
+
+	}
+
+
+}
+?>
